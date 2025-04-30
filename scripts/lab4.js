a18beb7d744e65c804cd9c86c193c0051766a6a9
Arlene Garcia <146496596+arlenecse@users.noreply.github.com>
2025-04-29 22:33:09 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -24,14 +24,14 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (prices.length == 0 || typeof prices !== typeof discount) {
+        return false;
+    }
     const discounted = []
     const length = prices.length;
     for(let i = 0; i < length; i++) {
         let discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
-    }
-    if (prices.length == 0 || typeof prices !== typeof discount) {
-        return false;
     }
     return discounted;
 }
